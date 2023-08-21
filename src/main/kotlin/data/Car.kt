1d13e71a64de1cb77b30b303f2f0b61d154b59c7
muhith7 <abdulmuhithnawawi@gmail.com>
2023-08-22 01:12:05 +0700

    Initializer Block

--- src/main/kotlin/data/Car.kt
+++ src/main/kotlin/data/Car.kt
@@ -1,6 +1,13 @@
 package data
 
 class Car(paramBrand: String, paramYear: Int = 2020){
+
+    //Initializer Block merupakan program yang bisa ditambahkan dalam class untuk dieksekusi setelah consturctor dijalankan
+    //Jangan pernah membuat program initializer yang kompleks karena akan memberatkan jalannya class , buatlah yang simple jika tidak perlu makan jnagan dibuat
+    init {
+        println("Produk $paramBrand ini dibuat pada tahun $paramYear")
+    }
+
     var brand: String = paramBrand
     var year: Int = paramYear
 }
