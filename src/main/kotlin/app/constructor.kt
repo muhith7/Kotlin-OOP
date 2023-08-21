c478079d1aa5c16656779815e6712ff341160cb3
muhith7 <abdulmuhithnawawi@gmail.com>
2023-08-22 04:44:09 +0700

    Secondary Constructor

--- src/main/kotlin/app/constructor.kt
+++ src/main/kotlin/app/constructor.kt
@@ -4,9 +4,9 @@ import data.Car
 
 //Constructor merupakan paramaeter pada class, dimana ketika kita membuat object baru maka constructor yang pertama kali yang harus di eksekusi
 fun main() {
-    val toyota = Car("Evolution Lancer X", 2015)
+    val toyota = Car("Evolution")
 
-    val honda = Car("Civic Type R", 2022)
+    val honda = Car("Civic","Type R")
 
     println(toyota.brand)
     println(toyota.year)
--- src/main/kotlin/data/Car.kt
+++ src/main/kotlin/data/Car.kt
@@ -1,6 +1,6 @@
 package data
 
-class Car(paramBrand: String, paramYear: Int = 2020){
+class Car(paramBrand: String, paramTipe: String, paramYear: Int = 2020){
 
     //Initializer Block merupakan program yang bisa ditambahkan dalam class untuk dieksekusi setelah consturctor dijalankan
     //Jangan pernah membuat program initializer yang kompleks karena akan memberatkan jalannya class , buatlah yang simple jika tidak perlu makan jnagan dibuat
@@ -8,7 +8,18 @@ class Car(paramBrand: String, paramYear: Int = 2020){
         println("Produk $paramBrand ini dibuat pada tahun $paramYear")
     }
 
+    //Secondary Constructor
+    //Constructor 2 this -> Constructor 1 -> Car
+    constructor(paramBrand: String,paramTipe: String): this (paramBrand,paramTipe,2020){
+        println("Secondary Constructor 1")
+    }
+
+    constructor(paramBrand: String): this (paramBrand, ""){
+        println("Secondary Constructor 2")
+    }
+
     var brand: String = paramBrand
+    var tipe: String = paramTipe
     var year: Int = paramYear
 }
 //Jika ingin membuat nilai default pada properti lebih baik menggunakan constructor agar lebih mudah merubahnya dalam object
